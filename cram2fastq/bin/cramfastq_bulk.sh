e04d916f5eac7a09c9f08720743321a7f917d6ca
Kelvin <kt16@sanger.ac.uk>
2021-10-18 14:34:59 +0100

    first commit

---
+++ cram2fastq/bin/cramfastq_bulk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+#discriminate between old and new CRAM files based on the presence of @SQ lines in the CRAM header
+if [[ `samtools view -H $1 | grep '@SQ' | wc -l` == 0 ]]
+then
+        #new file, no alignment, can just go straight for FASTQ
+        samtools fastq -1 $1\_R1_001.fastq.gz -2 $1\_R2_001.fastq.gz $1
+else
+        #old file with alignment, needs bamcollate2 incantation
+        # samtools view -b $1 | bamcollate2 collate=1 reset=1 resetaux=0 auxfilter=RG,BC,QT | samtools fastq -1 $1\_R1_001.fastq.gz -2 $1\_R2_001.fastq.gz -n
+        samtools collate -O -u $1 $1\_tmppfx | samtools fastq -N -F 0x900 -1 $1\_R1_001.fastq.gz -2 $1\_R2_001.fastq.gz -
+fi
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt", "rt", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh.readlines()]
+
+setup(
+    name="cram2fastq",
+    version= '0.0.1',
+    author="zktuong",
+    author_email="kt16@sanger.ac.uk",
+    description="Download and convert cram-to-fastq.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clatworthylab/cram2fastq/",
+    packages=find_packages(),
+    setup_requires=["setuptools>=45", "wheel", "setuptools_scm[toml]>=6.0"],
+    install_requires=requirements,
+    package_data={'cram2fastq': ['bin/cramfastq.sh', 'bin/cramfastq_bulk.sh']},
+    data_files=[('bin', ['bin/cramfastq.sh', 'bin/cramfastq_bulk.sh'])],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    zip_safe=False
+)
